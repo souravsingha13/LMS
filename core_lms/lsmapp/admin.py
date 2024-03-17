b3edb45eea12bc1264ca6e64080890a9c86aae96
souravsingha13 <souravsingha@uatech.biz>
2024-03-17 21:45:06 +0600

    :construction: added  auther model

--- core_lms/lsmapp/admin.py
+++ core_lms/lsmapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book,Author
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    empty_value_display = "None"
+    list_display = [field.name for field in Author._meta.fields if field.name != 'author_id']
+    fields = [field.name for field in Author._meta.fields if field.name != 'author_id']    
+
+class BookAdmin(admin.ModelAdmin):
+    empty_value_display = "None"
+    list_display = [field.name for field in Book._meta.fields if field.name != 'book_id']
+    fields = [field.name for field in Book._meta.fields if field.name != 'book_id']
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Book,BookAdmin)
+
--- core_lms/lsmapp/models.py
+++ core_lms/lsmapp/models.py
@@ -1,3 +1,39 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+
+class Author(models.Model):
+    author_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("Author Name", max_length=50)
+    nationality = models.CharField("Nationality", max_length=50)
+    birth_year = models.DateField(null=True, blank=True, editable=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Book(models.Model):
+    book_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(
+        "Book Name",
+        max_length=50,
+    )
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, max_length=50)
+    gener = models.CharField("Gener", max_length=50, null=True, blank=True)
+    publication_year = models.DateField(null=True, blank=True, editable=True)
+    book_img = models.ImageField(
+        "Book Image", null=True, blank=True, help_text="Provide a book image.",upload_to='media/images/'
+    )
+    description = models.CharField(
+        "Description",
+        null=True,
+        blank=True,
+        max_length=200,
+        help_text="Provide a short summary of this book",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}"
+
+
+

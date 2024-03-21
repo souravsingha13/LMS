7ec52ba5c450ca21ce1562984f6c91754ccff837
souravsingha13 <souravsingha@uatech.biz>
2024-03-21 19:06:38 +0600

    :Thread: added serializer for author create,delete update

--- core_lms/lsmapp/serializers.py
+++ core_lms/lsmapp/serializers.py
@@ -1,18 +1,26 @@
-from .models import Author,Book,Loan
+import uuid
+from .models import Author, Book, Loan
 from rest_framework import serializers
 
 
 class AuthonSerializer(serializers.Serializer):
-    author_id = serializers.UUIDField()
-    name = serializers.CharField(max_lenght = 50)
+    author_id = serializers.UUIDField(required=False, allow_null=True)
+    name = serializers.CharField(max_length=50)
     nationality = serializers.CharField(max_length=50)
     birth_year = serializers.DateField(allow_null=True, required=False)
-    
-    def create(self, validated_data):
-        author = Author.objects.create(**validated_data)
+    print("hello")
+
+    def create(self, validated_data) -> Author:
+        print("print from create")
+        try:
+            author = Author.objects.create(**validated_data)
+        except Exception as e:
+            raise e
         return author
 
     def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
-    
-
+        instance.name = validated_data.get("name", instance.name)
+        instance.nationality = validated_data.get("nationality", instance.nationality)
+        instance.birth_year = validated_data.get("birth_year", instance.birth_year)
+        instance.save()
+        return instance

3cbb1464b04875c28f5e338a36f9af99ab3d24e5
souravsingha13 <souravsingha@uatech.biz>
2024-04-02 23:54:05 +0600

    :construction: added swagger

--- core_lms/lsmapp/views/book_views.py
+++ core_lms/lsmapp/views/book_views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import exceptions, status
 from ..serializers.bookserializer import BookSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+import uuid
 
 
 class BookViewSet(ViewSet):
@@ -14,6 +16,9 @@ class BookViewSet(ViewSet):
     Provides CRUD (Create, Retrieve, Update, Delete) operations for books.
     """
 
+    @extend_schema(
+        responses=BookSerializer(many=True), description="Retrieve all books."
+    )
     def list(self, request: Request):
         try:
             books = Book.objects.all()
@@ -23,3 +28,75 @@ class BookViewSet(ViewSet):
             )
         serialized_books = BookSerializer(books, many=True)
         return Response(serialized_books.data, status=status.HTTP_200_OK)
+
+    @extend_schema(
+        request=BookSerializer,
+        responses=BookSerializer(),
+        description="Create a new book.",
+    )
+    def create(self, request: Request):
+        serialized_data = BookSerializer(data=request.data)
+
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="id", type=str, description="UUID of the book", location="path"
+            ),
+        ],
+        responses=BookSerializer(),
+        description="Retrieve a single book by its ID.",
+    )
+    def retrive(self, request: Response, id: uuid.uuid4):
+        try:
+            book = Book.objects.get(book_id=id)
+        except Book.DoesNotExist:
+            raise exceptions.NotFound(
+                f"Book with id {id} not found.", status=status.HTTP_404_NOT_FOUND
+            )
+        serialized_data = BookSerializer(data=book)
+        return Response(serialized_data.data)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="id", type=str, description="UUID of the book", location="path"
+            )
+        ],
+        request=BookSerializer,
+        responses=BookSerializer(),
+        description="Update a Book.",
+    )
+    def update(self, request: Request, id: uuid.uuid4):
+        try:
+            book_instance = Book.objects.get(book_id=id)
+        except Book.DoesNotExist:
+            raise exceptions.NotFound(f"Book with id {id} not found.")
+        serialized_data = BookSerializer(instance=book_instance, data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="id", type=str, description="UUID of the book", location="path"
+            ),
+        ],
+        description="Delete a book by its ID.",
+    )
+    def delete(self, request: Request, pk:uuid.uuid4):
+        try:
+            book_instance = Book.objects.get(book_id=pk)
+        except Book.DoesNotExist:
+            raise exceptions.NotFound(f"Book with id {pk} not found.")
+        book_instance.delete()
+        return Response(
+            f"Book with id {id} deleted successfully", status=status.HTTP_204_NO_CONTENT
+        )

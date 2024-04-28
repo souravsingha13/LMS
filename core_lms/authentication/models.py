1396fdb6c4281e781bd2cc4e167594ed238d8f85
souravsingha13 <souravsingha1304021@gmail.com>
2024-04-28 16:38:56 +0600

    :construction: Build a custom user model

--- core_lms/authentication/models.py
+++ core_lms/authentication/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+      
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email

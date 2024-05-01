11d9debb78795995db9cc37ba9d7198182569637
souravsingha13 <souravsingha@uatech.biz>
2024-05-01 23:46:17 +0600

    :wrench: added email send config

--- core_lms/core_lms/settings.py
+++ core_lms/core_lms/settings.py
@@ -136,6 +136,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+#Email verification setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT= 587 
+EMAIL_HOST_USER = "souravsingha1304021@gmail.com"
+EMAIL_HOST_PASSWORD = "najikaimodorkkya"
+EMAIL_USE_TLS = True
+
 
 LOGGING = {
     "version": 1,
